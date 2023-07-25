57563c74569d30955beab383893f5e6a3819d6be
Tony Zhou <tzhou49@gmail.com>
2023-07-25 14:07:29 -0400

    fix clearing whole list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,9 +3,8 @@ import { firebaseSlugBase } from "../utils/firebase";
 import { child, onValue, set } from "@firebase/database";
 import './Todo.css'
 
-export default function Todo({ localParticipant, participants }) {
+export default function Todo({ localParticipant }) {
     const [todo, setTodo] = useState([])
-    // const [sharedTodo, setSharedTodo] 
     const [newTodo, setNewTodo] = useState(['', false])
     const [editing, setEditing] = useState([false, -1])
     const [count, setCount] = useState(0)
@@ -15,19 +14,16 @@ export default function Todo({ localParticipant, participants }) {
         if (localParticipant) {
             set(child(base, `todo`), [...todo, newTodo])
         }
-        // setTodo([...todo, newTodo])
         setNewTodo(['', false])
         setCount(count+1)
     }
     const removeTodo = (i) => {
         const base = firebaseSlugBase();
         if (localParticipant) {
-            set(child(base, `todo`), todo.filter((val, indx) => 
-                indx !== i
-            ))
+            let res = todo.filter((val, indx) => indx !== i)
+            set(child(base, `todo`), res)
         }
         setCount(count-1)
-        // setTodo(todo.filter((val, indx) => indx !== i))
     }
 
     const editEntry = ()=> {
@@ -51,31 +47,39 @@ export default function Todo({ localParticipant, participants }) {
         }
         setCount(count+1)  
     }
-
+    const clearAll = () => {
+        const base = firebaseSlugBase();
+        if (localParticipant) {
+            set(child(base, `todo`), [])
+        }
+        setCount(0)        
+    }
     useEffect(() => {
         const base = firebaseSlugBase();
         const todoRef = child(base, "todo");
         onValue(todoRef, (snapshot) => {
-            if (snapshot.val()) {
-                let temp = []
+            let x = snapshot.val()
+            let temp = []
+            if (x) {
                 Object.entries(snapshot.val()).forEach(([key, val]) => {
                     if(val){
                         temp.push(val)
                     }
                 })
-                setTodo(temp);
             }
+            setTodo(temp);
         });
     }, [count]);
 
     return (
         <>
             <h3>Todo:</h3>
-            <div className='container'>
+            <div>
                 {todo !== null ? todo.map((item, indx) => {
                     let hideEdit = indx === editing[1] && editing[0]
                     return (
                         <div key={indx}>
+                            <label htmlFor={`todo-${indx}`}>Done</label>
                             <input type="checkbox" id={`todo-${indx}`} checked={item[1]} onChange={()=>toggleCheck(indx)}></input>
                             <input type="text" value={item[0]} readOnly={!hideEdit} onChange={(e) => setTodo(todo.map((val, i) => {
                                 let res = val[0]
@@ -95,5 +99,6 @@ export default function Todo({ localParticipant, participants }) {
                 <input type="text" value={newTodo[0]} onChange={e => setNewTodo([e.target.value, false])} id="newTodo" placeholder="New Item"></input>
                 <button onClick={addTodo} disabled={newTodo[0] === ''}>Add</button>
             </div>
+            <button onClick={clearAll}>Clear All</button>
         </>)
 }
